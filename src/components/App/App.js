2854394c7adbc84025e681c4e10226cc52fca94e
Pavel Tankovich <p.tankovich@andersenlab.com>
2020-03-24 22:49:31 +0300

    init app

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <header>
+        <img src="" />
+      </header>
+      <sidebar>
+          <nav>
+            <span>Profile</span>
+            <span>Messages</span>
+          </nav>
+      </sidebar>
+      <main>
+        Main content
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
